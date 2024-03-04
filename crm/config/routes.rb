ce531fc1d2bea8c85b8274608c2415e5d31c249d
dennyg8 <dennisg1999@outlook.com>
2024-03-04 14:42:02 -0600

    Added new controllers

--- crm/config/routes.rb
+++ crm/config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get 'customers/show'
   ActiveAdmin.routes(self)
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: 'customers#index'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+
   resources :customers, only: [:index]
+  get 'customers/alphabetized', to: 'customers#alphabetized'
+  get 'customers/missing_email', to: 'customers#missing_email'
 end
